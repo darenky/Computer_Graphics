b5bcf12f3aaa69c03d73534a03cb832f4d0ec0e2
darenky <kuyesdaren@gmail.com>
2024-04-07 11:40:50 +0300

    Final touch

--- Lab3/animation.py
+++ Lab3/animation.py
@@ -24,40 +24,38 @@ def plot_axiom(ax, axiom, fi, dfi, l):
             fi += dfi
         elif axiom[i] == '-':
             fi -= dfi
-    ax.plot(x, y, 'k')  
+    ax.plot(x, y, 'b')  
 
 def animate_fractal(axiom, rules, min_angle, max_angle, num_frames, max_iter, filename):
     fig, ax = plt.subplots()
-    ax.set_aspect('equal', 'box') # 'equal" - axes have the same scaling
 
     def update(frame):
         ax.clear()  
         current_angle = min_angle + (max_angle - min_angle) * frame / num_frames
         result = test_tur(axiom, rules, max_iter, current_angle)  
         plot_axiom(ax, result, 0, current_angle, 1)
-        ax.set_title(f"Angle: {current_angle:.2f}") # '.2f' - round angle to 2 decimals
+        ax.set_title(f"Angle: {current_angle:.2f}") 
 
-    writer = PillowWriter(fps=2)   # fps = frames per second
-    ani = FuncAnimation(fig, update, frames=num_frames, repeat=False)
-    ani.save(filename, writer=writer)  
+    writer = PillowWriter(fps=10)   # fps = frames per second
+    anim = FuncAnimation(fig, update, frames=num_frames, repeat=False)
+    anim.save(filename, writer=writer)  
     plt.close(fig)  
 
-# same for all fractals
+
 num_frames = 30 
 max_iter = 5
 fi = 0
 
-# fractals data (name, axiom, rules, dfi)
 fractals = [
-    ("Koch Snowflake", "F++F++F", {"F": "F-F++F-F"}, np.pi/3),
-    ("Sierpinski Cross (Хрестоподібний фрактал)", "F+XF+F+XF", {"X": "XF-F+F-XF+F+XF-F+F-X"}, np.pi/2),
-    ("Crystal Snowflake (Кристалоподібний фрактал)", "F+F+F+F", {"F": "FF+F++F+F"}, np.pi/2),
-    ("Fractal Star (Фрактальна зірочка)", "X+X+X+X+X+X+X+X", {"X": "X+YF++YF-FX--FXFX-YF+X", "Y": "-FX+YFYF++YF+FX--FX-YF"}, np.pi/4),
-    ("Serpinski Arrowhead (Наконечник стріли Серпінського)", "YF", {"X": "YF+XF+Y", "Y": "XF-YF-X"}, np.pi/3),
-    ("Fractal Basin (Фрактальний басейн)", "-D--D", {"A": "F++FFFF--F--FFFF++F++FFFF--F", "B": "F--FFFF++F++FFFF--F--FFFF++F", "C": "BFA--BFA", "D": "CFC--CFC"}, np.pi/4),
-    ("Fractal Rings (Фрактальні кільця)", "F+F+F+F", {"F": "FF+F+F+F+F+F-F"}, np.pi/2),
-    ("Dragon Curve (Крива дракона)", "FX", {"X": "X+YF+", "Y": "-FX-Y"}, np.pi/2)
-]
+     ("Koch Snowflake", "F++F++F", {"F": "F-F++F-F"}, np.pi/3),
+    ("Sierpinski Cross", "F+XF+F+XF", {"X": "XF-F+F-XF+F+XF-F+F-X"}, np.pi/2),
+    ("Crystal Snowflake", "F+F+F+F", {"F": "FF+F++F+F"}, np.pi/2),
+    ("Fractal Star", "X+X+X+X+X+X+X+X", {"X": "X+YF++YF-FX--FXFX-YF+X", "Y": "-FX+YFYF++YF+FX--FX-YF"}, np.pi/4),
+    ("Serpinski Arrowhead", "YF", {"X": "YF+XF+Y", "Y": "XF-YF-X"}, np.pi/3),
+    ("Fractal Basin", "-D--D", {"A": "F++FFFF--F--FFFF++F++FFFF--F", "B": "F--FFFF++F++FFFF--F--FFFF++F", "C": "BFA--BFA", "D": "CFC--CFC"}, np.pi/4),
+    ("Fractal Rings", "F+F+F+F", {"F": "FF+F+F+F+F+F-F"}, np.pi/2),
+    ("Dragon Curve", "FX", {"X": "X+YF+", "Y": "-FX-Y"}, np.pi/2)
+ ]
 
 
 for name, axiom, rules, dfi in fractals:
