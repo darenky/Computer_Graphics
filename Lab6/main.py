9eafc82e7c87cf1105f694692cd66aee6790502c
darenky <kuyesdaren@gmail.com>
2024-05-21 12:24:02 +0300

    add lab6

---
+++ Lab6/main.py
@@ -0,0 +1,52 @@
+from PIL import Image
+import numpy as np
+
+def convert_to_binary_image(image_path):
+    image = Image.open(image_path).convert('1')  # Convert to binary
+    return np.array(image)
+
+watermark_binary = convert_to_binary_image('WM.png')
+
+def embed_watermark(input_image_path, watermark_binary, bit_plane):
+    image = Image.open(input_image_path)
+    image_array = np.array(image)
+    
+    watermark_rows, watermark_cols = watermark_binary.shape
+    img_rows, img_cols, _ = image_array.shape
+    
+    for i in range(img_rows):
+        for j in range(img_cols):
+            watermark_pixel = watermark_binary[i % watermark_rows, j % watermark_cols] # Ensures that the indices i and j wrap around when they exceed the dimensions of the watermark
+            bit = 1 if watermark_pixel else 0
+            
+            # Embed bit in the specified bit plane of the blue channel
+            image_array[i, j, 2] = (image_array[i, j, 2] & ~(1 << (bit_plane - 1))) | (bit << (bit_plane - 1))
+    
+    return Image.fromarray(image_array)
+
+embedded_image = embed_watermark('img.png', watermark_binary, 8)  # Specify the bit plane (1-8)
+embedded_image.save('embedded_watermark_image.png')
+
+def text_to_bits(text):
+    return ''.join(format(ord(char), '08b') for char in text)
+
+def embed_text(input_image_path, text):
+    image = Image.open(input_image_path)
+    image_array = np.array(image)
+    
+    binary_text = text_to_bits(text)
+    data_index = 0
+    text_length = len(binary_text)
+    
+    for i in range(image_array.shape[0]):
+        for j in range(image_array.shape[1]):
+            if data_index < text_length:
+                image_array[i, j, 2] = (image_array[i, j, 2] & 0xFE) | int(binary_text[data_index])
+                data_index += 1
+    
+    return Image.fromarray(image_array)
+
+embedded_text_image = embed_text('embedded_watermark_image.png', "Hidden text here")
+embedded_text_image.save('8bits.png')
+
+
